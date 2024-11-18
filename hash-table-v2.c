1313947456b681fe91328e709115cb3c620ee848
Natalie Choo <83897877+nataliechoo@users.noreply.github.com>
2024-11-18 09:30:05 -0800

    clean ver

--- hash-table-v2.c
+++ hash-table-v2.c
@@ -83,6 +83,7 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
                              const char *key,
                              uint32_t value)
 {
+	//HOW DO I FURTHER OPTIMIZE WHEN I ALREADY MAKE A LOCK PER ENTRY //
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
 	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
